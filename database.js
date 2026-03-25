10139d3ad8cc12d9d882f7d4d66e3356d8b7feec
adam <aross27@byu.edu>
2026-03-25 17:36:32 -0600

    Add MongoDB connection setup in a new database.js file and add mongodb dependency.

---
+++ database.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require('mongodb');
+const config = require('./service/dbConfig.json');
+
+const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('simon');
+
+const userCollection = db.collection('users');
+const taskCollection = db.collection('tasks');
+const settingsCollection = db.collection('settings');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log('Connected to database');
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+module.exports = {};
